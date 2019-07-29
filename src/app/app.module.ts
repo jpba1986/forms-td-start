b367ae2912c3604802bc5f12bd738deedbcc0fa3
Juan Pablo Barria Aguila <juan.barria@falp.org>
2019-07-29 09:21:37 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
