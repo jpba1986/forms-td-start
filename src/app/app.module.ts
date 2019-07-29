e57e0aefdeccacea06b4eb10a800027f7d979790
Juan Pablo Barria Aguila <juan.barria@falp.org>
2019-07-29 11:13:56 -0400

    first strike

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgModel } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -13,7 +13,7 @@ import { UserComponent } from './user/user/user.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    FormsModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/user/user/user.component.ts
+++ src/app/user/user/user.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  @ViewChild('f', {static: false}) singupForm : NgForm;
+  email        ='';
+  pass         ='';
+  subscription ='';
+  subs         ='';
+  defaultSubs  ='advanced';
+  showData = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(){
+    this.showData = true;
+    this.email =this.singupForm.value.email;
+    this.pass  =this.singupForm.value.pass;
+    this.subs  = this.singupForm.value.subscription;
+    this.singupForm.reset();/**/
+    console.log(this.singupForm);
   }
 
 }
