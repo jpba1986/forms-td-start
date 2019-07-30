4ce14ff1e3ddc7c41053513d190b34b9bccd988d
Juan Pablo Barria Aguila <juan.barria@falp.org>
2019-07-30 10:07:24 -0400

    creating FormControl ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -10,8 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
+    BrowserModule,    
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
